a61e104ce05faa5fc88e37a8079d255ac9d85da8
TrodsKey <resh@live.dk>
2017-10-29 22:08:52 +0100

    added scene changer
    it is now possible to change scenes, just add scenechanger script to the relevant button

---
+++ Assets/sceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class sceneChanger : MonoBehaviour {
+
+    public Toggle thisButton;
+    public string targetSceneName;
+
+    public void changeTheScene(bool whatever)
+    {
+        //Debug.Log("changing scene");
+        SceneManager.LoadScene(targetSceneName);
+    }
+
+    void Start()
+    {
+        thisButton.onValueChanged.AddListener(changeTheScene);
+    }
+
+}
