3904f172bfbc18b6c62907b7f1718e20823968d3
TrodsKey <resh@live.dk>
2017-10-31 10:47:06 +0100

    Ramme nærmest færdig

---
+++ Assets/fadeInPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeInPanel : MonoBehaviour {
+
+    public Vector3 startMarker = new Vector3 (-100, 120, 0);
+    public Vector3 endMarker = new Vector3 (48, 120, 0);
+    public float smoothing = 1f;
+
+    private void OnDisable()
+    {
+        transform.position = startMarker;
+    }
+
+    private void OnEnable()
+    {
+        //StartCoroutine(FadeIn(smoothing));
+        transform.position = endMarker;
+    }
+
+    private IEnumerator FadeIn(float fadeTime)
+    {
+        while (Vector3.Distance(startMarker, endMarker) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(startMarker, endMarker, fadeTime * Time.deltaTime);
+
+            yield return null;
+        }
+        print("Reached the target.");
+
+        yield return new WaitForSeconds(3f);
+
+        print("MyCoroutine is now finished.");
+    }
+}
--- Assets/sceneChanger.cs
+++ Assets/sceneChanger.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class sceneChanger : MonoBehaviour {
 
-    public Toggle thisButton;
     public string targetSceneName;
 
+    //unity complains if the parameter isn't there ¯\_(ツ)_/¯
     public void changeTheScene(bool whatever)
     {
         //Debug.Log("changing scene");
         SceneManager.LoadScene(targetSceneName);
-    }
-
-    void Start()
-    {
-        thisButton.onValueChanged.AddListener(changeTheScene);
     }
 
 }
